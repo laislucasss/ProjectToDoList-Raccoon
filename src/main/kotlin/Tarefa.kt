56b90b92d2626b8a052694c8603da01eed6f9b01
Lais Ferreira <laisluccas@hotmail.com>
2021-08-17 13:57:40 -0300

    update e delete tarefa

---
+++ src/main/kotlin/Tarefa.kt
@@ -0,0 +1,15 @@
+class Tarefa {
+    val id: Int
+    var status: String
+    var descricao: String
+
+    constructor(id: Int, status: String, descricao: String) {
+        this.id = id
+        this.status = status
+        this.descricao = descricao
+    }
+}
+
+
+
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,58 @@
 // [X] Ver lista
 // [X] Criar tarefa
-// [X] Atualizar tarefa
+// [] Atualizar tarefa
   // [] Tratar quando item nao existir
 // [X] Deletar tarefa
-  // [] Tratar quando item nao existir
+  // [X] Tratar quando item nao existir
 
 
-fun printLista(lista: List<String>) {
+fun printLista(lista: List<Tarefa>) {
     for (item in lista) {
-        println(item)
+        println("${item.id} ${if (item.status == "CONCLUÍDO") "[X]" else "[ ]" } ${item.descricao}")
     }
 }
-fun atualizaLista(lista: ArrayList<String>, itemAntigo: String, itemNovo: String) {
-    val indexItemAntigo = lista.indexOf(itemAntigo)
-    lista[indexItemAntigo] = itemNovo
+fun atualizaTarefa(lista: ArrayList<Tarefa>, novaTarefa: Tarefa) {
+    var tarefa = lista.find { tarefa -> tarefa.id == novaTarefa.id }
+    tarefa = novaTarefa
 }
 
-fun removeLista(lista: ArrayList<String>, item: String) {
-    lista.remove(item)
+fun removeTarefa(lista: ArrayList<Tarefa>, id: Int) {
+    lista.removeIf { tarefa -> tarefa.id == id }
+}
+
+fun criaTarefa(lista: ArrayList<Tarefa>, status: String, descricao: String) : Tarefa {
+    if (lista.isEmpty()) {
+        var novoId = 1
+        var tarefa = Tarefa(novoId, status, descricao)
+        lista.add(tarefa)
+        return tarefa
+    }
+
+
+    var ultimoElemento = lista.last()
+    var novoId = ultimoElemento.id +1
+    var tarefa = Tarefa(novoId, status, descricao)
+    lista.add(tarefa)
+    return tarefa
 }
 
 
 fun main(args: Array<String>) {
-    val listaTarefas = ArrayList<String>()
-    listaTarefas.add("lais")
-    listaTarefas.add("tais")
+    val listaTarefas = ArrayList<Tarefa>()
+    var t1 = criaTarefa(listaTarefas, "FAZENDO", "Comprar pão")
+    var t2 = criaTarefa(listaTarefas,"PENDENTE", "Comprar shampoo")
+    var t3 = criaTarefa(listaTarefas, "CONCLUÍDO", "Comprar detergente")
+
 
     printLista(listaTarefas)
-    atualizaLista(listaTarefas, "tais", "lucas")
+    println("-----")
+
+    t2.status = "CONCLUÍDO"
+    atualizaTarefa(listaTarefas, t2)
     printLista(listaTarefas)
-    removeLista(listaTarefas, "lucas")
+    println("-----")
+
+    removeTarefa(listaTarefas, 1)
     printLista(listaTarefas)
 }
 
